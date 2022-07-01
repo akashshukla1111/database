1fdb7c8725234b392873bfa8a1e0be93c9a614fc
a0s01hy <akash.shukla@walmart.com>
2022-07-01 10:00:00 +0530

    added

--- shellscripting/cert.sh
+++ shellscripting/cert.sh
@@ -2,3 +2,4 @@
 #cat /etc/secrets/gls-atlas-fes-api-kafka.nonprod.walmart.com.jks > /etc/secrets/SslKeyStoreLocation.txt
 cat /etc/secrets/gls-atlas-order-tracker-kafka.nonprod.walmart.com.jks > /etc/secrets/SslTruststoreLocation.txt
 cat /etc/secrets/gls-atlas-order-tracker-kafka.nonprod.walmart.com.jks > /etc/secrets/SslKeyStoreLocation.txt
+#liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath=/Users/a0s01hy/Downloads/apache-tomcat-9.0.52/webapps/ROOT/WEB-INF/lib/mssql-jdbc-8.4.1.jre8.jar --changeLogFile=./initial-ddl.sql --url="jdbc:sqlserver://localhost:1433;database=NDOF_TRIP_EXEC" --username=sa --password=A_Str0ng_Required_Password update
