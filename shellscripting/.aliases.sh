7f011f3712430f5655c2718bd4de4ec0886bb4e3
a0s01hy <akash.shukla@walmart.com>
2025-07-26 02:15:01 +0530

    changes done in shell script

--- shellscripting/.aliases.sh
+++ shellscripting/.aliases.sh
@@ -10,36 +10,55 @@ alias b='fs -frs wms-receiving,gdm,os,aos,nte,loading/fc,default-fc,us-wm-fc fc-
 # Environment specific aliases
 alias qa='fs -frs wms-receiving,gdm,os,aos,nte,loading/fc,us-wm-fc us-wm-fc.yml | search qa -s'
 alias default='fs -frs wms-receiving,gdm,os,aos,nte,loading/fc,default-fc fc-pre-main-merge.yml | search default,stg -s'
-# Individual service aliases
-alias rcv='fs -frs wms-receiving/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search -s'
-alias gdm='fs -frs gdm/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search -s'
-alias inv='fs -fr isc/us-wm-fc,default-fc us-wm-fc.yml,pre-main.yml | ss -s'
-alias os='fs -frs os/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search -s'
-alias aos='fs -frs aos/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search -s'
-alias fes='fs -frs nte/default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search -s'
-alias loading='fs -frs loading-server/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search -s'
 
+function rcv() {
+  echo "fs -frs wms-receiving/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s"
+  fs -frs wms-receiving/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s
+}
+function gdm() {
+  echo "fs -frs gdm/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s"
+  fs -frs gdm/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s
+}
+function inv() {
+  echo "fs -fr isc/us-wm-fc,default-fc us-wm-fc.yml,pre-main.yml | ss  $1 -s"
+  fs -fr isc/us-wm-fc,default-fc us-wm-fc.yml,pre-main.yml | ss $1 -s
+}
+function os() {
+  echo "fs -frs os/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s"
+  fs -frs os/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s
+}
+function aos() {
+  echo "fs -frs aos/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s"
+  fs -frs aos/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s
+}
+function fes() {
+  echo "fs -frs nte/default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s"
+  fs -frs nte/default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s
+}
+function loading() {
+  echo "fs -frs loading-server/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s"
+  fs -frs loading-server/fc,default-fc,us-wm-fc fc-pre-main-merge.yml,us-wm-fc.yml | search $1 -s
+}
 
-function vp() {
-  case "$1" in
+function v() {
+  case "$2" in
     i)
-      echo "fs -frs wms-receiving,gdm,os,aos,nte,loading,isc,uli/fc,us-wm-fc us-wm-fc.yml | ss $2 -s"
-      fs -frs wms-receiving,gdm,isc,slotting,uwms-location/fc,us-wm-fc us-wm-fc.yml | ss $2 -s;;
+      echo "fs -frs wms-receiving,gdm,os,aos,nte,loading,isc,uli/fc,us-wm-fc us-wm-fc.yml | ss $1 -s"
+      fs -frs wms-receiving,gdm,isc,slotting,uwms-location/fc,us-wm-fc us-wm-fc.yml | ss $1 -s;;
     o)
-      echo "fs -frs os,aos,nte,loading,isc/fc,us-wm-fc us-wm-fc.yml | ss $2 -s"
-      fs -frs os,aos,nte,loading,uli/fc,us-wm-fc us-wm-fc.yml | ss $2 -s;;
+      echo "fs -frs os,aos,nte,loading,isc/fc,us-wm-fc us-wm-fc.yml | ss $1 -s"
+      fs -frs os,aos,nte,loading,uli/fc,us-wm-fc us-wm-fc.yml | ss $1 -s;;
     ui)
-      echo "fs -frs gdm-web,idc,decant-api/us-wm-fc,us-wm-manual-fc us-wm-manual-fc.yml,us-wm-fc.yml | ss $2 -s"
-      fs -frs gdm-web,idc,decant-api/us-wm-fc,us-wm-manual-fc us-wm-manual-fc.yml,us-wm-fc.yml | ss $2 -s ;;
+      echo "fs -frs gdm-web,idc,decant-api/us-wm-fc,us-wm-manual-fc us-wm-manual-fc.yml,us-wm-fc.yml | ss $1 -s"
+      fs -frs gdm-web,idc,decant-api/us-wm-fc,us-wm-manual-fc us-wm-manual-fc.yml,us-wm-fc.yml | ss $1 -s ;;
     io)
-      echo "fs -frs wms-receiving,gdm,isc,slotting,uwms-location,os,aos,nte,loading,isc,uli/fc,us-wm-fc us-wm-fc.yml | ss $2 -s"
-            fs -frs wms-receiving,gdm,isc,slotting,uwms-location,os,aos,nte,loading,isc,uli/fc,us-wm-fc us-wm-fc.yml | ss $2 -s;;
+      echo "fs -frs wms-receiving,gdm,isc,slotting,uwms-location,os,aos,nte,loading,isc,uli/fc,us-wm-fc us-wm-fc.yml | ss $1 -s"
+            fs -frs wms-receiving,gdm,isc,slotting,uwms-location,os,aos,nte,loading,isc,uli/fc,us-wm-fc us-wm-fc.yml | ss $1 -s;;
     ioui)
-      echo "fs -frs wms-receiving,gdm,os,aos,nte,loading,isc,uli,idc,decant-api/fc,us-wm-fc,us-wm-manual-fc us-wm-fc.yml,us-wm-manual-fc.yml | ss $2 -s"
-      fs -frs wms-receiving,gdm,isc,slotting,uwms-location,os,aos,nte,loading,uli,idc,decant-api/fc,us-wm-fc,us-wm-manual-fc us-wm-fc.yml,us-wm-manual-fc.yml | ss $2 -s;;
+      echo "fs -frs wms-receiving,gdm,os,aos,nte,loading,isc,uli,idc,decant-api/fc,us-wm-fc,us-wm-manual-fc us-wm-fc.yml,us-wm-manual-fc.yml | ss $1 -s"
+      fs -frs wms-receiving,gdm,isc,slotting,uwms-location,os,aos,nte,loading,uli,idc,decant-api/fc,us-wm-fc,us-wm-manual-fc us-wm-fc.yml,us-wm-manual-fc.yml | ss $1 -s;;
     *)
-      echo "Usage: vp {i|o|ui|io|ioui} <arg>"
-      return 1
-      ;;
+     echo "fs -frs wms-receiving,gdm,os,aos,nte,loading,isc,uli,idc,decant-api/fc,us-wm-fc,us-wm-manual-fc us-wm-fc.yml,us-wm-manual-fc.yml | ss $1 -s"
+           fs -frs wms-receiving,gdm,isc,slotting,uwms-location,os,aos,nte,loading,uli,idc,decant-api/fc,us-wm-fc,us-wm-manual-fc us-wm-fc.yml,us-wm-manual-fc.yml | ss $1 -s;;
   esac
 }
